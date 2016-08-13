ee778dcd9354af3e047612d6a05a8d3b2a96166f
EBiddlecome <elizabethwbiddlecome@gmail.com>
2016-08-13 14:15:31 -0700

    cleaned up nice

--- ReverseASentence.js
+++ ReverseASentence.js
@@ -1,19 +1,26 @@
-function reverse(sentence) {
+//display the words in a sentence in reverse order
+
+var reverseSentence = function (sentence) {
+
 	//split the sentence into words, using spaces as separator
-	var words = sentence.split(" ");
+  var words = sentence.split(' ');
+
 	//define caching variable
-	var reversed = " ";
+  var reversed = ' ';
+
 	//iterate backwards through the words in the sentence
-	for (var i = words.length - 1; i >= 0; i--){
+  for (var i = words.length - 1; i >= 0; i--) {
+
 		//add each word to the cache
-		reversed += words[i] + " ";
-	}
+    reversed += words[i] + ' ';
+  }
+
 	//return cache contents, which will be the words listed in reverse
-	return reversed;
+  return reversed;
 };
 
+//test case
+var phrase = 'When the hoomans are away, the dogs will play';
 
-var phrase =  "When the hoomans are away, the dogs will play";
-
-console.log(reverse(phrase));
+console.log(reverseSentence(phrase));
 

39beb37505559d9ecba8e03724948c90a49f61ff
E Biddlecome <EBiddlecome@users.noreply.github.com>
2016-08-23 19:17:29 -0700

    Update isPermutation.js

--- isPermutation.js
+++ isPermutation.js
@@ -1,4 +1,4 @@
-// Given two strings,write a method to decide if one is a permutation of the other.
+// Given two strings, write a method to decide if one is a permutation of the other.
 
 
 //define test cases:
